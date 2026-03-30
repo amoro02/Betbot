7664cec8821d5124db3474fb5a41a4c564ff61d7
amoro02 <arturomoro02@gmail.com>
2026-03-29 23:40:09 -0500

    Create main React dashboard component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Betbot Dashboard</h1>
+            {/* Additional components can be added here */}
+        </div>
+    );
+};
+
+export default App;
